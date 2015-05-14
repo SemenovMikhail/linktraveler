d8d3e2ec79068331082b66fcbf554541ab6dfd23
SemenovMikhail <fknisit@gmail.com>
2015-05-14 19:54:21 +0400

    merge

--- test.php
+++ test.php
@@ -1,8 +1,5 @@
 <?php
-
-include '/var/www/html/linktraveler/lib/simple_html_dom.php';
-echo "123";
-$d = @file_get_html("http://google.com");
-	echo($d);
+$headers = @get_headers("http://google.com");
+var_dump($headers);
 ?>
 	
