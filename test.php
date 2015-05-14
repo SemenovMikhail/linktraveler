a243618e55501d21fca6f83b39773e657aea08e0
root <root@linktraveler.ru>
2015-05-14 12:49:01 -0400

    functional link travel

--- test.php
+++ test.php
@@ -1,8 +1,12 @@
 <?php
 
-include '/var/www/html/linktraveler/lib/simple_html_dom.php';
-echo "123";
-$d = @file_get_html("http://google.com");
-	echo($d);
+include '/var/www/html/linktraveler/scripts/linktraveler.php';
+
+error_reporting(E_ALL ^ E_NOTICE);	
+$replace = "http://www.hellsangelscostablanca.es/links.html"; 
+echo $replace;
+if(check_url_for_errors($replace))
+	return 0;
+else echo "good";
 ?>
 	
